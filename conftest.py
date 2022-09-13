46a9618f49b44960edc72510c8c5d1a7616d9e28
romansumkin <sumkin.romanw@gmail.com>
2022-09-13 18:24:45 +0400

    Minor fix of time sleep and empty lines

--- conftest.py
+++ conftest.py
@@ -31,7 +31,3 @@ def browser(request):
     yield browser
     print("\nquit browser..")
     browser.quit()
-
-
-
-
--- test_items.py
+++ test_items.py
@@ -7,5 +7,5 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 def test_add_to_busket_button_is_on_page(browser):
     browser.get(link)
     assert browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket")
-    time.sleep(5)
+    time.sleep(30)
 
